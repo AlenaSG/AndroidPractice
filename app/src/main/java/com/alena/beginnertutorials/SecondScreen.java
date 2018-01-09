c92a363f4adc634a420515b8f316374049553dda
Alena Golovina <alenasgolovina@gmail.com>
2018-01-09 14:38:58 -0800

    add scrollView for large text

--- app/src/main/java/com/alena/beginnertutorials/SecondScreen.java
+++ app/src/main/java/com/alena/beginnertutorials/SecondScreen.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by alenagolovina on 11/29/17.
@@ -19,9 +20,24 @@ public class SecondScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_screen_layout);
+
         Log.d(TAG, "onCreate: second screen started");
 
+        TextView mTitleWindow = (TextView) findViewById(R.id.titleWindow);
+        TextView mMessageWindow = (TextView) findViewById(R.id.messageWidow);
         Button btnNavToFirst = (Button) findViewById(R.id.btnGoToFirstScreen);
+
+        mTitleWindow.setText("This is a title.");
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String someMessage = " This is some message to append. ";
+
+        for (int i=0; i < 100; i++) {
+            stringBuilder.append(someMessage);
+        }
+
+        mMessageWindow.setText(stringBuilder.toString());
 
         btnNavToFirst.setOnClickListener(new View.OnClickListener() {
             @Override
