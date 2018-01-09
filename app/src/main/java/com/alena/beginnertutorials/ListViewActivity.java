b128a86eceaf743ca44dca834e2e33a01cc6c99b
Alena Golovina <alenasgolovina@gmail.com>
2018-01-08 16:34:32 -0800

    add listView filter and search bar

--- app/src/main/java/com/alena/beginnertutorials/ListViewActivity.java
+++ app/src/main/java/com/alena/beginnertutorials/ListViewActivity.java
@@ -3,8 +3,11 @@ package com.alena.beginnertutorials;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,12 +18,14 @@ import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
     private static final String TAG = "ListViewActivity";
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         ListView list = (ListView) findViewById(R.id.theList);
+        EditText theFilter = (EditText) findViewById(R.id.searchFilter);
         Log.d(TAG, "onCreate: started ListView Act");
 
         ArrayList<String> names = new ArrayList<>();
@@ -33,7 +38,24 @@ public class ListViewActivity extends AppCompatActivity {
 //using different layouts
        // ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, names);
        // list.setAdapter(adapter);
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
+        adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
         list.setAdapter(adapter);
+
+        theFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (ListViewActivity.this).adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
