c7d245a3eaf0c69157059c1f3b8c3e5cb096f7f5
Alena Golovina <alenasgolovina@gmail.com>
2018-01-12 16:41:03 -0800

    set onItemClickListener for a simple ListView

--- app/src/main/java/com/alena/beginnertutorials/ListViewActivity.java
+++ app/src/main/java/com/alena/beginnertutorials/ListViewActivity.java
@@ -6,29 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-
-/**
- * Created by alenagolovina on 11/29/17.
- */
 
 public class ListViewActivity extends AppCompatActivity {
     private static final String TAG = "ListViewActivity";
     private ArrayAdapter adapter;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
         ListView list = (ListView) findViewById(R.id.theList);
+
         EditText theFilter = (EditText) findViewById(R.id.searchFilter);
         Log.d(TAG, "onCreate: started ListView Act");
 
-        ArrayList<String> names = new ArrayList<>();
+        //declare ArrayList as final so the onClick listener cn see it
+        final ArrayList<String> names = new ArrayList<>();
         names.add("Alena");
         names.add("Anna");
         names.add("Sowmya");
@@ -40,6 +41,14 @@ public class ListViewActivity extends AppCompatActivity {
        // list.setAdapter(adapter);
         adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
         list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, "onItemClick: name: " + names.get(i));
+                Toast.makeText(ListViewActivity.this, "You clicked on: " + names.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         theFilter.addTextChangedListener(new TextWatcher() {
             @Override
